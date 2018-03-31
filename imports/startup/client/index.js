202eeb34433f15682a2a2d39b6d1952d62ffcd56
Rob Secord <rob.secord@instant.co>
2018-03-30 20:43:37 -0400

    updates

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -35,5 +35,5 @@ if (location.host !== 'localhost:3000' && location.host !== '127.0.0.1:3000' && 
 Meteor.startup(function clientIndexStartup() {
     log.log('OwnTheDay Version 1.5.6');
     log.log('Check out our Ethereum Contract here: https://etherscan.io/address/0x16d790ad4e33725d44741251f100e635c323beb9#code');
-    log.log('Have a great Day!');
+    log.log('Have a great Day! 📆 🚀');
 });
